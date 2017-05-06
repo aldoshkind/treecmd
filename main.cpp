dc6bad351e2a9a5b7dd4595d170985dc137f4ec3
Dmitry <aldoshkind@yandex.ru>
2017-05-06 23:21:48 +0700

    Tree added; added commands: mk ls rm

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+#include "tree/node.h"
+
 typedef std::vector<std::string> tokens_t;
 typedef void (*command_t)(const tokens_t &);
 
@@ -16,6 +18,34 @@ typedef std::map<std::string, command_t>		commands_t;
 commands_t										commands;
 
 std::string current_node_path = "/";
+
+node root;
+
+std::string absolute_path(std::string p)
+{
+	if(p.size() == 0)
+	{
+		return p;
+	}
+
+	clean_path(p);
+
+	auto dot_pos = p.find_first_of('.');
+
+	if(dot_pos != std::string::npos)
+	{
+		p = p.substr(0, dot_pos);
+	}
+
+	if(p[0] == '/')
+	{
+		return p;
+	}
+
+	p = current_node_path + "/" + p;
+	clean_path(p);
+	return p;
+}
 
 void pwd(const tokens_t &)
 {
@@ -30,13 +60,43 @@ void cd(const tokens_t &t)
 	}
 	else
 	{
-		current_node_path = t[1];
+		std::string path = absolute_path(t[1]);
+		node *n = root.at(path);
+		if(n != nullptr)
+		{
+			current_node_path = path;
+		}
+		else
+		{
+			printf("does not exist\n");
+		}
 	}
 }
 
-void ls(const tokens_t &)
+void ls(const tokens_t &t)
 {
-	//
+	std::string path;
+
+	if(t.size() < 2)
+	{
+		path = current_node_path;
+	}
+	else
+	{
+		path = t[1];
+	}
+	node *n = root.at(path);
+	if(n == nullptr)
+	{
+		printf("%s does not exist\n", path.c_str());
+		return;
+	}
+	node::ls_list_t items = n->ls();
+	printf("total: %d items\n", items.size());
+	for(auto item : items)
+	{
+		printf("%s\n", item.c_str());
+	}
 }
 
 void set(const std::string &path, const std::string &value)
@@ -134,19 +194,75 @@ void help(const tokens_t &)
 	}
 }
 
+void mknode(const tokens_t &t)
+{
+	std::string status;
+	for(tokens_t::size_type i = 1 ; i < t.size() ; i += 1)
+	{
+		std::string path = absolute_path(t[i]);
+
+		node *n = root.at(path);
+
+		if(n != nullptr)
+		{
+			status = "exists";
+		}
+		else
+		{
+			n = root.append(path);
+			if(n != nullptr)
+			{
+				status = "created";
+			}
+			else
+			{
+				status = "creation failed";
+			}
+		}
+
+		printf("%s %s\n", path.c_str(), status.c_str());
+	}
+}
+
+void rm(const tokens_t &t)
+{
+	std::string status;
+	for(tokens_t::size_type i = 1 ; i < t.size() ; i += 1)
+	{
+		std::string path = absolute_path(t[i]);
+
+		node *n = root.at(path);
+
+		if(n == nullptr)
+		{
+			status = "does not exist";
+		}
+		else
+		{
+			root.remove(path, true);
+			status = "removed";
+		}
+
+		printf("%s %s\n", path.c_str(), status.c_str());
+	}
+}
+
 void init_commands()
 {
 	commands["exit"] = exit_cmd;
 	commands["help"] = help;
 	commands["pwd"] = pwd;
 	commands["cd"] = cd;
+	commands["mk"] = mknode;
+	commands["ls"] = ls;
+	commands["rm"] = rm;
 }
 
 int main()
 {
 	init_commands();
 
-	char *input, shell_prompt[100] = " > ";
+	char *input, shell_prompt[] = " > ";
 	rl_bind_key('\t', tab);
 
 	for( ; ; )
--- treecmd.pro
+++ treecmd.pro
@@ -3,6 +3,13 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    tree/filepath_utils.cpp \
+    tree/resource.cpp
 
 LIBS += -lreadline
+
+HEADERS += \
+    tree/filepath_utils.h \
+    tree/node.h \
+    tree/resource.h
