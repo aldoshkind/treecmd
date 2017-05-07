5ce1167b02140710419bd95b42d2de0cd27d9c00
Dmitry <aldoshkind@yandex.ru>
2017-05-07 19:16:56 +0700

    Setting and getting value infrastructure + minor improvements

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <sstream>
 
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -14,8 +15,17 @@
 typedef std::vector<std::string> tokens_t;
 typedef void (*command_t)(const tokens_t &);
 
+typedef int (*value_setter_t)(property_base *p, const std::string &value);
+typedef int (*value_getter_t)(property_base *p, std::string &value);
+
 typedef std::map<std::string, command_t>		commands_t;
 commands_t										commands;
+
+typedef std::map<std::string, value_setter_t> setters_t;
+typedef std::map<std::string, value_getter_t> getters_t;
+
+setters_t setters;
+getters_t getters;
 
 std::string current_node_path = "/";
 
@@ -25,7 +35,7 @@ std::string absolute_path(std::string p)
 {
 	if(p.size() == 0)
 	{
-		return p;
+		return current_node_path;
 	}
 
 	if(p[0] == '.')
@@ -129,13 +139,131 @@ void lsprops(const tokens_t &t)
 	}
 }
 
-void set(const std::string &path, const std::string &value)
+int setter_double(property_base *p, const std::string &value)
 {
-	printf("%s : %s\n", path.c_str(), value.c_str());
+	property<double> *pd = dynamic_cast<property<double> *>(p);
+
+	if(pd == NULL)
+	{
+		return -1;
+	}
+
+	pd->set_value(strtod(value.c_str(), NULL));
+
+	return 0;
+}
+
+int getter_double(property_base *p, std::string &value)
+{
+	property<double> *pd = dynamic_cast<property<double> *>(p);
+
+	if(pd == NULL)
+	{
+		return -1;
+	}
+
+	std::stringstream render;
+	render << pd->get_value();
+	value = render.str();
+
+	return 0;
+}
+
+void set(node *n, std::string &prop, std::string value)
+{
+	property_base *p = n->get_property(prop);
+	if(p == NULL)
+	{
+		printf("property %s does not exist\n", prop.c_str());
+		return;
+	}
+
+	setters_t::iterator it = setters.find(p->get_type());
+	if(it == setters.end())
+	{
+		printf("dont know how to handle value of type %s\n", p->get_type().c_str());
+		return;
+	}
+	int res = (it->second)(p, value);
+	if(res != 0)
+	{
+		printf("error setting value\n");
+		return;
+	}
+
+	//printf("set property %s has type %s\n", prop.c_str(), p->get_type().c_str());
+}
+
+void set(const std::string &target, const std::string &value)
+{
+	std::string::size_type dot_pos = target.find_last_of('.');
+	if(dot_pos == std::string::npos)
+	{
+		printf("property not specified\n");
+		return;
+	}
+	std::string path = target.substr(0, dot_pos);
+	std::string prop_name = target.substr(dot_pos + 1);
+
+	node *n = root.at(absolute_path(path));
+	if(n == nullptr)
+	{
+		printf("%s does not exist\n", path.c_str());
+		return;
+	}
+	set(n, prop_name, value);
+}
+
+void print(const std::string &target)
+{
+	std::string::size_type dot_pos = target.find_last_of('.');
+	if(dot_pos == std::string::npos)
+	{
+		printf("property not specified\n");
+		return;
+	}
+	std::string path = target.substr(0, dot_pos);
+	std::string prop = target.substr(dot_pos + 1);
+
+	node *n = root.at(absolute_path(path));
+	if(n == nullptr)
+	{
+		printf("%s does not exist\n", path.c_str());
+		return;
+	}
+
+	property_base *p = n->get_property(prop);
+	if(p == NULL)
+	{
+		printf("property %s does not exist\n", prop.c_str());
+		return;
+	}
+
+	getters_t::iterator it = getters.find(p->get_type());
+	if(it == getters.end())
+	{
+		printf("dont know how to handle value of type %s\n", p->get_type().c_str());
+		return;
+	}
+	std::string value;
+	int res = (it->second)(p, value);
+	if(res != 0)
+	{
+		printf("error getting value\n");
+		return;
+	}
+	printf("%s = %s\n", target.c_str(), value.c_str());
+
+	//printf("print property %s has type %s\n", prop.c_str(), p->get_type().c_str());
 }
 
 void eval(const tokens_t &tok)
 {
+	if(tok.size() == 0)
+	{
+		return;
+	}
+
 	std::string expr;
 	for(tokens_t::size_type i = 0 ; i < tok.size() ; i += 1)
 	{
@@ -143,7 +271,13 @@ void eval(const tokens_t &tok)
 	}
 
 	std::string::size_type ass_sign_pos = expr.find_last_of("=");
-	std::string path = expr.substr(0, ass_sign_pos - 1);
+	if(ass_sign_pos == std::string::npos)
+	{
+		print(tok[0]);
+		return;
+	}
+
+	std::string path = expr.substr(0, ass_sign_pos);
 	std::string value = expr.substr(ass_sign_pos + 1);
 
 	set(path, value);
@@ -277,7 +411,21 @@ void rm(const tokens_t &t)
 	}
 }
 
-void init_commands()
+void types(const tokens_t &/*t*/)
+{
+	printf("set:\n");
+	for(auto setter : setters)
+	{
+		printf("\t%s\n", setter.first.c_str());
+	}
+	printf("get:\n");
+	for(auto getter : getters)
+	{
+		printf("\t%s\n", getter.first.c_str());
+	}
+}
+
+void init()
 {
 	commands["exit"] = exit_cmd;
 	commands["help"] = help;
@@ -287,15 +435,21 @@ void init_commands()
 	commands["ls"] = ls;
 	commands["rm"] = rm;
 	commands["lsprops"] = lsprops;
+	commands["types"] = types;
+
+	setters["d"] = setter_double;
+	getters["d"] = getter_double;
 }
 
 int main()
 {
-	init_commands();
+	init();
 
 	node *tmp = root.append("tmp");
 	tmp->add_property(new property_value<double>("roll"));
 	tmp->add_property(new property_value<double>("pitch"));
+	tmp->add_property(new property_value<int>("mode"));
+	tmp->add_property(new property_value<std::string>("state"));
 	root.append("a/b/c/d/e/f");
 	current_node_path = "/a/b/c/d/e/f";
 
@@ -315,7 +469,10 @@ int main()
 		tokens_t ts = tokenize(input);
 		exec(ts);
 
-		add_history(input);
+		if(strlen(input) != 0)
+		{
+			add_history(input);
+		}
 		free(input);
 	}
 }
